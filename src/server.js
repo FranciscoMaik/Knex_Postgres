eace1133e57e870e331ae348b5f119c24430ac35
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-12 00:38:33 -0300

    Finalizando crud de usuarios

--- src/server.js
+++ src/server.js
@@ -3,5 +3,20 @@ const app = express();
 
 const routes = require("./routes");
 
+app.use(express.json());
 app.use(routes);
+
+//notFound
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+//catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
 app.listen(3333, () => console.log("Server is running"));
