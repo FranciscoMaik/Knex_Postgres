4ebbf63b333ec7d3235fa84aeb5d07276ef19bd4
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-12 03:11:58 -0300

    alteraçao do server

--- src/server.js
+++ src/server.js
@@ -19,4 +19,4 @@ app.use((error, req, res, next) => {
   res.json({ error: error.message });
 });
 
-app.listen(process.env.PORT || 3333, () => console.log("Server is running"));
+app.listen(process.env.PORT || 3333);
